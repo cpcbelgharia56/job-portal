1a91a6793ad1b865649282e39d8e225eb0edf2b7
cpcbelgharia56 <cpcbelgharia56@gmail.com>
2024-07-18 00:35:45 +0530

    Update apiList.js
    Update live backend url.

--- frontend/src/lib/apiList.js
+++ frontend/src/lib/apiList.js
@@ -1,4 +1,4 @@
-export const server = "http://localhost:4444";
+export const server = "https://job-portal-backend-pt80.onrender.com"
 //export const server = "https://secret-shelf-87665.herokuapp.com";
 
 const apiList = {
